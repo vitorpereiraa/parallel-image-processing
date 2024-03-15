cf35ef54f2c08de9c43cb856400c0ab6c9c41259
Barbatos-Rex <59457214+Barbatos-Rex@users.noreply.github.com>
2024-03-15 22:02:27 +0000

    Add project structure

---
+++ src/main/java/ApplyFilters.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ApplyFilters {
+ 
+    public static void main(String[] args) throws IOException {
+		
+		Scanner input = new Scanner(System.in);        
+		String filePath = "";
+		System.out.println("Insert the name of the file path you would like to use.");
+        filePath = input.nextLine();
+        input.close();
+        Filters filters = new Filters(filePath);
+        filters.BrighterFilter("brighter.jpg",128);
+    }
+
+}
