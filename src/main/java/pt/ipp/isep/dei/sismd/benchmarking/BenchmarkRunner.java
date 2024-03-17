a91e6f95bd89379147bdba209df37d6bd0d69224
dm16147 <domingosmachado2001@gmail.com>
2024-03-17 14:36:23 +0000

    Benchmark
    Adding benchmarks for Glass Filter Sequential and Multithreaded + Results

---
+++ src/main/java/pt/ipp/isep/dei/sismd/benchmarking/BenchmarkRunner.java
@@ -0,0 +1,8 @@
+package pt.ipp.isep.dei.sismd.benchmarking;
+
+public class BenchmarkRunner {
+
+    public static void main(String[] args) throws Exception {
+        org.openjdk.jmh.Main.main(args);
+    }
+}
