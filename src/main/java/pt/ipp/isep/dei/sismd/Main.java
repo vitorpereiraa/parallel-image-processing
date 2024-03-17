ee57e5a80b7818ff3a070c3693e54b4beae05743
dm16147 <domingosmachado2001@gmail.com>
2024-03-17 13:41:55 +0000

    Glass Filter Multithreaded solution
    Glass Filter Multithreaded solution

---
+++ src/main/java/pt/ipp/isep/dei/sismd/Main.java
@@ -0,0 +1,32 @@
+package pt.ipp.isep.dei.sismd;
+
+import pt.ipp.isep.dei.sismd.domain.Image;
+import pt.ipp.isep.dei.sismd.filter.BrighterFilter;
+import pt.ipp.isep.dei.sismd.filter.GlassFilter;
+import pt.ipp.isep.dei.sismd.filter.GlassFilterMultithreaded;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner input = new Scanner(System.in);
+        String filePath = "";
+        System.out.println("Insert the name of the file path you would like to use.");
+        filePath = input.nextLine();
+        input.close();
+
+        Image image = Utils.loadImage(filePath);
+        Image transformedImage = new GlassFilterMultithreaded().apply(image);
+
+        File outputDir = new File("./out");
+        if (outputDir.isDirectory() && !outputDir.exists()) {
+            outputDir.mkdirs();
+        }
+        Utils.writeImage(transformedImage, "brighter.jpg");
+    }
+
+}
---
+++ src/main/java/pt/ipp/isep/dei/sismd/filter/GlassFilterMultithreaded.java
@@ -0,0 +1,94 @@
+package pt.ipp.isep.dei.sismd.filter;
+
+import pt.ipp.isep.dei.sismd.domain.Color;
+import pt.ipp.isep.dei.sismd.domain.Image;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class GlassFilterMultithreaded implements ImageFilter {
+
+    private int distance = 40;
+    private int numberOfThreads = 5;
+
+    private AtomicInteger numberOfRowsLeft;
+
+    public GlassFilterMultithreaded(int distance, int numberOfThreads) {
+        this.distance = distance;
+        this.numberOfThreads = numberOfThreads;
+    }
+
+    public GlassFilterMultithreaded() {
+    }
+
+    public int getRow() {
+        int row = this.numberOfRowsLeft.get();
+        this.numberOfRowsLeft = new AtomicInteger(row - 1);
+        return row;
+    }
+
+    @Override
+    public Image apply(Image image) {
+        int numberOfRows = image.height();
+        this.numberOfRowsLeft = new AtomicInteger(numberOfRows - 1);
+        int numberOfColumns = image.width();
+
+        Color[][] pixelMatrix = new Color[numberOfRows][numberOfColumns];
+        
+        List<Thread> threadList = new ArrayList<>();
+        for(int i = 0; i < numberOfThreads; i++) {
+            threadList.add(new Thread(new GlassFilterRunnable(numberOfColumns, numberOfRows, pixelMatrix, image)));
+        }
+
+        for (Thread thread : threadList) {
+            thread.start();
+        }
+
+        for (Thread thread : threadList) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return new Image(pixelMatrix);
+    }
+
+    private class GlassFilterRunnable implements Runnable {
+
+        private final Color[][] sharedOutput;
+        private final Image imageToProcess;
+
+        private final int numberOfColumns;
+        private final int numberOfRows;
+
+        private GlassFilterRunnable(int numberOfColumns, int numberOfRows, Color[][] sharedOutput, Image imageToProcess) {
+            this.sharedOutput = sharedOutput;
+            this.imageToProcess = imageToProcess;
+            this.numberOfColumns = numberOfColumns;
+            this.numberOfRows = numberOfRows;
+        }
+
+        @Override
+        public void run() {
+            Random rand = new Random();
+            int row;
+            while((row = getRow()) >= 0) {
+                for (int j = 0; j < numberOfColumns; j++) {
+                    int offsetI = rand.nextInt(distance) - distance * 2;
+                    int offsetJ = rand.nextInt(distance) - distance * 2;
+
+                    int randomI = Math.min(Math.max(0,row + offsetI), numberOfRows - 1);
+                    int randomJ = Math.min(Math.max(0,j + offsetJ), numberOfColumns - 1);
+
+                    Color color = imageToProcess.obtainPixel(randomI, randomJ);
+
+                    sharedOutput[row][j]=color;
+                }
+            }
+        }
+    }
+}
