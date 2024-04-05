8655769c5e2e59d8469ad9453404ef46b6ec1c3c
Afonso Machado <1190326@isep.ipp.pt>
2024-04-05 22:34:46 +0100

    Conditional Blur Filter alg

---
+++ src/main/java/pt/ipp/isep/dei/sismd/filter/ConditionalBlurFilter.java
@@ -0,0 +1,47 @@
+package pt.ipp.isep.dei.sismd.filter;
+
+import pt.ipp.isep.dei.sismd.domain.Color;
+import pt.ipp.isep.dei.sismd.domain.Image;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ConditionalBlurFilter implements ImageFilter {
+
+    int m = 1;
+    @Override
+    public Image apply(Image image) {
+       Color[][] pixelMatrix = new Color[image.height()][image.width()];
+        for (int i = 0; i < image.height()-1; i++) {
+            for (int j = 0; j < image.width()-1; j++) {
+                Color color = image.obtainPixel(i, j);
+                List<Color> neighboursAndSelfValues = getNeighboursAndSelfValues(image, i, j, m);
+                int r = neighboursAndSelfValues.stream().map(Color::red).reduce(0, Integer::sum) / neighboursAndSelfValues.size();
+                int g = neighboursAndSelfValues.stream().map(Color::green).reduce(0, Integer::sum) / neighboursAndSelfValues.size();
+                int b = neighboursAndSelfValues.stream().map(Color::blue).reduce(0, Integer::sum) / neighboursAndSelfValues.size();
+
+                if(r > 200) {
+                    pixelMatrix[i][j] = new Color(r, g, b);
+                } else {
+                    pixelMatrix[i][j] = new Color(color.red(), color.green(), color.blue());
+                }
+            }
+        }
+
+        return new Image(pixelMatrix);
+    }
+
+    private List<Color> getNeighboursAndSelfValues(Image image, int i, int j, int m) {
+
+        List<Color> neighboursAndSelfValues = new ArrayList<>();
+        for (int k = i - m; k <= i + m; k++) {
+            for (int l = j - m; l <= j + m; l++) {
+                if (k >= 0 && k < image.height() && l >= 0 && l < image.width() && (k != i || l != j)) {
+                    neighboursAndSelfValues.add(image.obtainPixel(k, l));
+                }
+            }
+        }
+        return neighboursAndSelfValues;
+    }
+}
