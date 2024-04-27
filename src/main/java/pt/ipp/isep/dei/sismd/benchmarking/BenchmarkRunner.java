e33d448cdfee7c5065b1abd1ccb17df6e732c564
André Gomes <1190387@isep.ipp.pt>
2024-04-27 18:08:04 +0100

    Add Bechmarking to Blur filters

--- src/main/java/pt/ipp/isep/dei/sismd/benchmarking/BenchmarkRunner.java
+++ src/main/java/pt/ipp/isep/dei/sismd/benchmarking/BenchmarkRunner.java
@@ -1,9 +1,7 @@
 package pt.ipp.isep.dei.sismd.benchmarking;
 
 import org.openjdk.jmh.runner.RunnerException;
-import pt.ipp.isep.dei.sismd.filters.BrighterFilter;
-import pt.ipp.isep.dei.sismd.filters.GrayscaleFilter;
-import pt.ipp.isep.dei.sismd.filters.SwirlFilter;
+import pt.ipp.isep.dei.sismd.filters.*;
 
 public class BenchmarkRunner extends BaseBenchmarkRunner {
 
@@ -11,5 +9,7 @@ public class BenchmarkRunner extends BaseBenchmarkRunner {
         runBaseBenchmark(BrighterFilter.class.getName(), true);
         runBaseBenchmark(GrayscaleFilter.class.getName(), true);
         runBaseBenchmark(SwirlFilter.class.getName(), true);
+        runBaseBenchmark(ConditionalBlurFilter.class.getName(), true);
+        runBaseBenchmark(BlurFilter.class.getName(), true);
     }
 }
